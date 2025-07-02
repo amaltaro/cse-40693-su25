2f0dabc380aa216a3350c4aaa405e0347f45a477
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-07-01 23:52:42 -0400

    Provide a WorkflowTable module for the table creation

--- src/Components/Main/MainList.js
+++ src/Components/Main/MainList.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getAllWorkflows } from '../../Services/WorkflowService.js';
+import WorkflowTable from './WorkflowTable.js';
 
 const MainList = () => {
     const [workflows, setWorkflows] = useState([]);
@@ -16,22 +17,7 @@ const MainList = () => {
 
     return (
         <div>
-            {workflowArray.map((workflow, index) => (
-                <tr key={workflow.id}>
-                    <th scope="row">{index + 1}</th>
-                    <td data-column="workflow">
-                        {workflow.get('RequestName')}
-                    </td>
-                    <td data-column="status">{workflow.get('RequestStatus')}</td>
-                    <td data-column="type">{workflow.get('RequestType')}</td>
-                    <td data-column="priority">{workflow.get('RequestPriority')}</td>
-                    <td data-column="created">{workflow.get('jobs_created')}</td>
-                    <td data-column="pending">{workflow.get('jobs_pending')}</td>
-                    <td data-column="running">{workflow.get('jobs_running')}</td>
-                    <td data-column="success">{workflow.get('jobs_success')}</td>
-                    <td data-column="failure">{workflow.get('jobs_failed')}</td>
-                </tr>
-            ))}
+            <WorkflowTable workflows={workflowArray} />
         </div>
     );
 };
