60e047f32d7b7ee03210b75f6bec4423e77e0654
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-07-01 23:34:13 -0400

    Fix table syntax for workflow array

--- src/Components/Main/MainList.js
+++ src/Components/Main/MainList.js
@@ -11,26 +11,27 @@ const MainList = () => {
         });
     }, []);
 
-    console.log("state", w);
+    console.log("state", workflows);
     const workflowArray = Array.isArray(workflows) ? workflows : [];
+
     return (
         <div>
-            <tr>
-                {workflows.map(workflow => (
-                <th scope="row">${index + 1}</th>
-                <td key={workflow.id} data-column="workflow">
-                    {workflow.get('RequestName')}
-                </td>
-                <td data-column="status">${workflow.RequestStatus}</td>
-                <td data-column="type">${workflow.RequestType}</td>
-                <td data-column="priority">${workflow.RequestPriority}</td>
-                <td data-column="created">${workflow.jobs_created}</td>
-                <td data-column="pending">${workflow.jobs_pending}</td>
-                <td data-column="running">${workflow.jobs_running}</td>
-                <td data-column="success">${workflow.jobs_success}</td>
-                <td data-column="failure">${workflow.jobs_failed}</td>
-                ))}
-            </tr>
+            {workflowArray.map((workflow, index) => (
+                <tr key={workflow.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td data-column="workflow">
+                        {workflow.get('RequestName')}
+                    </td>
+                    <td data-column="status">{workflow.get('RequestStatus')}</td>
+                    <td data-column="type">{workflow.get('RequestType')}</td>
+                    <td data-column="priority">{workflow.get('RequestPriority')}</td>
+                    <td data-column="created">{workflow.get('jobs_created')}</td>
+                    <td data-column="pending">{workflow.get('jobs_pending')}</td>
+                    <td data-column="running">{workflow.get('jobs_running')}</td>
+                    <td data-column="success">{workflow.get('jobs_success')}</td>
+                    <td data-column="failure">{workflow.get('jobs_failed')}</td>
+                </tr>
+            ))}
         </div>
     );
 };
