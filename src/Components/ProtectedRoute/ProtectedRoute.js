5771c73c5d33a702419b08d2d1e0cd4ff5d9f3df
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-07-19 15:06:21 -0400

    Refactor ProtectedRoute to simply rediret unauthenticated user to /auth

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { checkUser } from "../../Services/Credentials";
-import "./ProtectedRoute.css";
 
 // You can pass props using the spread operator to throw them on an object
 // if there are too many to break out
@@ -9,29 +8,19 @@ const ProtectedRoute = ({ element, ...rest }) => {
   console.log("element: ", element);
   const navigate = useNavigate();
 
-  const goBackHandler = () => {
-    navigate("/auth");
-  };
+  useEffect(() => {
+    if (!checkUser()) {
+      // Automatically redirect unauthenticated users to auth page
+      navigate("/auth");
+    }
+  }, [navigate]);
 
+  // Only render the protected element if user is authenticated
   if (checkUser()) {
     return element;
   } else {
-    return (
-      <div className="unauthorized-container">
-        <div className="unauthorized-card">
-          <div className="unauthorized-header">
-            <h1 className="unauthorized-title">Unauthorized!</h1>
-            <p className="unauthorized-subtitle">You need to be logged in to access this page.</p>
-          </div>
-          <div className="unauthorized-button-container">
-            <button onClick={goBackHandler} className="unauthorized-btn">
-              <i className="bi bi-arrow-left me-2"></i>
-              Go Back
-            </button>
-          </div>
-        </div>
-      </div>
-    );
+    // Return null while redirecting to prevent flash of unauthorized content
+    return null;
   }
 };
 
