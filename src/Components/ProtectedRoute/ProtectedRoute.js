1ea18a3f1042e69dc02d6664e7277433f35b023e
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-07-19 11:14:37 -0400

    Apply modern/responsive design to ProfileMenu and ProtectedRoute

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { checkUser } from "../../Services/Credentials";
+import "./ProtectedRoute.css";
 
 // You can pass props using the spread operator to throw them on an object
 // if there are too many to break out
@@ -16,8 +17,19 @@ const ProtectedRoute = ({ element, ...rest }) => {
     return element;
   } else {
     return (
-      <div>
-        <p>Unauthorized!</p> <button onClick={goBackHandler}>Go Back.</button>
+      <div className="unauthorized-container">
+        <div className="unauthorized-card">
+          <div className="unauthorized-header">
+            <h1 className="unauthorized-title">Unauthorized!</h1>
+            <p className="unauthorized-subtitle">You need to be logged in to access this page.</p>
+          </div>
+          <div className="unauthorized-button-container">
+            <button onClick={goBackHandler} className="unauthorized-btn">
+              <i className="bi bi-arrow-left me-2"></i>
+              Go Back
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
