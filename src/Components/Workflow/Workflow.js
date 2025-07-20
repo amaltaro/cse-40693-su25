f7076c4d74289ef64d546adb1dca31cc076d7ac1
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-07-19 20:13:26 -0400

    Add support to query parameters in the Workflow component

--- src/Components/Workflow/Workflow.js
+++ src/Components/Workflow/Workflow.js
@@ -1,19 +1,48 @@
 import { useEffect, useState } from "react";
-import { getAllWorkflows } from "../../Services/WorkflowService";
+import { useSearchParams } from "react-router-dom";
+import { getAllWorkflows, getWorkflowsByQuery } from "../../Services/WorkflowService";
 import Header from '../Header/Header';
 import WorkflowForm from "./WorkflowForm.js";
 import WorkflowList from "./WorkflowList.js";
 import { initializeTableFeatures } from "../../Services/TableFunctions.js";
 
 const Workflow = () => {
-console.log("Executing Workflow module");
-const [workflows, setWorkflows] = useState([]);
+    console.log("Executing Workflow module");
+    const [workflows, setWorkflows] = useState([]);
+    const [searchParams] = useSearchParams();
 
-useEffect(() => {
-    getAllWorkflows().then((workflows) => {
-    setWorkflows(workflows);
-    });
-}, []);
+    useEffect(() => {
+        // Extract query parameters from URL
+        const queryParams = {};
+
+        // Check for each possible query parameter
+        const workflow = searchParams.get('workflow');
+        const status = searchParams.get('status');
+        const type = searchParams.get('type');
+        const campaign = searchParams.get('campaign');
+        const cmssw = searchParams.get('cmssw');
+
+        // Only add parameters that are actually present
+        if (workflow) queryParams.workflow = workflow;
+        if (status) queryParams.status = status;
+        if (type) queryParams.type = type;
+        if (campaign) queryParams.campaign = campaign;
+        if (cmssw) queryParams.cmssw = cmssw;
+
+        console.log("Query parameters:", queryParams);
+
+        // If we have query parameters, use the filtered function
+        if (Object.keys(queryParams).length > 0) {
+            getWorkflowsByQuery(queryParams).then((workflows) => {
+                setWorkflows(workflows);
+            });
+        } else {
+            // Otherwise, get all workflows
+            getAllWorkflows().then((workflows) => {
+                setWorkflows(workflows);
+            });
+        }
+    }, [searchParams]); // Re-run when search params change
 
 // Initialize table features after workflows are loaded and rendered
 useEffect(() => {
@@ -25,11 +54,39 @@ useEffect(() => {
     }
 }, [workflows]);
 
-return (
-    <section>
-        <Header title="WM Workflow" />
-        <WorkflowForm />
-        {/* This table will eventually be populated with dynamic data */}
+    // Helper function to display current filters
+    const getActiveFilters = () => {
+        const filters = [];
+        if (searchParams.get('workflow')) filters.push(`Workflow: ${searchParams.get('workflow')}`);
+        if (searchParams.get('status')) filters.push(`Status: ${searchParams.get('status')}`);
+        if (searchParams.get('type')) filters.push(`Type: ${searchParams.get('type')}`);
+        if (searchParams.get('campaign')) filters.push(`Campaign: ${searchParams.get('campaign')}`);
+        if (searchParams.get('cmssw')) filters.push(`CMSSW: ${searchParams.get('cmssw')}`);
+        return filters;
+    };
+
+    const activeFilters = getActiveFilters();
+
+    return (
+        <section>
+            <Header title="WM Workflow" />
+            <WorkflowForm />
+
+            {/* Display active filters if any */}
+            {activeFilters.length > 0 && (
+                <div className="container-fluid mb-3">
+                    <div className="alert alert-info">
+                        <strong>Active Filters:</strong> {activeFilters.join(', ')}
+                        <span className="ms-2">
+                            <a href="/workflow" className="btn btn-sm btn-outline-secondary">
+                                Clear Filters
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            )}
+
+            {/* This table will eventually be populated with dynamic data */}
 <div className="container-fluid">
     <div className="table-responsive">
     <table
